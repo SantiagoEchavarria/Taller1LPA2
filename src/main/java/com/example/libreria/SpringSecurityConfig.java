860695cd8f38a1b831f3320363ab3e09be9cd324
SantiagoEchavarria <santiagogil7112@gmail.com>
2025-10-22 14:36:47 -0500

    Solucion de errores

--- src/main/java/com/example/libreria/SpringSecurityConfig.java
+++ src/main/java/com/example/libreria/SpringSecurityConfig.java
@@ -8,15 +8,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.example.libreria.models.service.UsuarioDetailService;
+//import com.example.libreria.models.service.UsuarioDetailService;
 
 @Configuration
 public class SpringSecurityConfig {
-    private final UsuarioDetailService usuarioDetailService;
+   /*  private final UsuarioDetailService usuarioDetailService;
 
     public SpringSecurityConfig(UsuarioDetailService usuarioDetailService){
         this.usuarioDetailService = usuarioDetailService;
-    }
+    }*/
 
     @Bean
     BCryptPasswordEncoder passwordEncoder(){
@@ -28,17 +28,18 @@ public class SpringSecurityConfig {
         return configuration.getAuthenticationManager();
     }
 
-
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(authz -> authz
         .requestMatchers("/", "/css/**", "/js/**", "/img/**", "/libreria/libroslistar").permitAll()
+        .requestMatchers("/libreria/usuarionuevo").hasAnyRole("USER")
+        .requestMatchers("/libreria/libroeditar/**", "/libreria/libroelimnar/**", "/libreria/libronuevo").hasAnyRole("ADMIN")
         .anyRequest().authenticated()
         )
         .formLogin(login -> login
             .loginPage("/login")
-            // .defaultSuccessUrl("/comercial/productoslistar", true)
-            .defaultSuccessUrl("/?loginSuccess", true)
+            .defaultSuccessUrl("/libreria/libroslistar", true)
+            //.defaultSuccessUrl("/?loginSuccess", true)
             .permitAll()
         ) 
         .logout(logout -> logout
